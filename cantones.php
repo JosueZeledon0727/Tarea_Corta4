87c100b6b1bdb6c897eb9013af9fe9b6746c9aaa
Josué Zeledón <josue.zeledon0727@gmail.com>
2019-10-24 16:45:31 -0600

    Cambios a los nombres de los cantones

--- cantones.php
+++ cantones.php
@@ -4,25 +4,25 @@
      case 1:
         $cantones = array(
            array('id' => 1, 'nombre' => 'Central'), 
-           array('id' => 2, 'nombre' => 'Escazú'), 
+           array('id' => 2, 'nombre' => 'Escazu'), 
            array('id' => 3, 'nombre' => 'Desamparados'),
            array('id' => 4, 'nombre' => 'Puriscal'),
-           array('id' => 5, 'nombre' => 'Tarrazú'),
-           array('id' => 6, 'nombre' => 'Aserrí'),
+           array('id' => 5, 'nombre' => 'Tarrazu'),
+           array('id' => 6, 'nombre' => 'Aserri'),
            array('id' => 7, 'nombre' => 'Mora'),
            array('id' => 8, 'nombre' => 'Goicoechea'),
            array('id' => 9, 'nombre' => 'Santa Ana'),
            array('id' => 10, 'nombre' => 'Alajuelita'),
-           array('id' => 11, 'nombre' => 'Vásquez de Coronado'),
+           array('id' => 11, 'nombre' => 'Vasquez de Coronado'),
            array('id' => 12, 'nombre' => 'Acosta'),
-           array('id' => 13, 'nombre' => 'Tibás'),
+           array('id' => 13, 'nombre' => 'Tibas'),
            array('id' => 14, 'nombre' => 'Moravia'),
            array('id' => 15, 'nombre' => 'Montes de Oca'),
            array('id' => 16, 'nombre' => 'Turrubares'),
            array('id' => 17, 'nombre' => 'Dota'),
            array('id' => 18, 'nombre' => 'Curridabat'),
-           array('id' => 19, 'nombre' => 'Pérez Zeledón'),
-           array('id' => 20, 'nombre' => 'León Cortés')
+           array('id' => 19, 'nombre' => 'Perez Zeledon'),
+           array('id' => 20, 'nombre' => 'Leon Cortes')
         );
         break;
      case 2:
@@ -35,11 +35,11 @@
            array('id' => 6, 'nombre' => 'Naranjo'),
            array('id' => 7, 'nombre' => 'Orotina'),
            array('id' => 8, 'nombre' => 'Palmares'),
-           array('id' => 9, 'nombre' => 'Poás'),
-           array('id' => 10, 'nombre' => 'Río Cuarto'),
+           array('id' => 9, 'nombre' => 'Poas'),
+           array('id' => 10, 'nombre' => 'Rio Cuarto'),
            array('id' => 11, 'nombre' => 'San Carlos'),
            array('id' => 12, 'nombre' => 'San Mateo'),
-           array('id' => 13, 'nombre' => 'San Ramón'),
+           array('id' => 13, 'nombre' => 'San Ramon'),
            array('id' => 14, 'nombre' => 'Upala'),
            array('id' => 15, 'nombre' => 'Valverde Vega'),
            array('id' => 16, 'nombre' => 'Zarcero') 
@@ -50,10 +50,10 @@
            array('id' => 1, 'nombre' => 'Cartago'), 
            array('id' => 2, 'nombre' => 'Alvarado'),
            array('id' => 3, 'nombre' => 'El Guarco'),
-           array('id' => 4, 'nombre' => 'Jiménez'),
-           array('id' => 5, 'nombre' => 'La Unión'),
+           array('id' => 4, 'nombre' => 'Jimenez'),
+           array('id' => 5, 'nombre' => 'La Union'),
            array('id' => 6, 'nombre' => 'Oreamuno'),
-           array('id' => 7, 'nombre' => 'Paraíso'),
+           array('id' => 7, 'nombre' => 'Paraiso'),
            array('id' => 8, 'nombre' => 'Turrialba')
         );
         break;
@@ -61,14 +61,14 @@
 		$cantones = array(
            array('id' => 1, 'nombre' => 'Heredia'), 
            array('id' => 2, 'nombre' => 'Barva'),
-           array('id' => 3, 'nombre' => 'Belén'),
+           array('id' => 3, 'nombre' => 'Belen'),
            array('id' => 4, 'nombre' => 'Flores'),
            array('id' => 5, 'nombre' => 'San Isidro'),
            array('id' => 6, 'nombre' => 'San Pablo'),
            array('id' => 7, 'nombre' => 'San Rafael'),
-           array('id' => 8, 'nombre' => 'Santa Bárbara'),
+           array('id' => 8, 'nombre' => 'Santa Barbara'),
            array('id' => 9, 'nombre' => 'Santo Domingo'),
-           array('id' => 10, 'nombre' => 'Sarapiquí')
+           array('id' => 10, 'nombre' => 'Sarapiqui')
         );
         break;
      case 5:
@@ -76,14 +76,14 @@
            array('id' => 1, 'nombre' => 'Liberia'), 
            array('id' => 2, 'nombre' => 'Abangares'),
            array('id' => 3, 'nombre' => 'Bagaces'),
-           array('id' => 4, 'nombre' => 'Cañas'),
+           array('id' => 4, 'nombre' => 'Canas'),
            array('id' => 5, 'nombre' => 'Carrillo'),
            array('id' => 6, 'nombre' => 'Hojancha'),
            array('id' => 7, 'nombre' => 'La Cruz'),
            array('id' => 8, 'nombre' => 'Nandayure'),
            array('id' => 9, 'nombre' => 'Nicoya'),
            array('id' => 10, 'nombre' => 'Santa Cruz'),
-           array('id' => 11, 'nombre' => 'Tilarán')
+           array('id' => 11, 'nombre' => 'Tilaran')
         );
         break;
         
@@ -105,10 +105,10 @@
         
      case 7:
 		$cantones = array(
-           array('id' => 1, 'nombre' => 'Limón'), 
-           array('id' => 2, 'nombre' => 'Guácimo'),
+           array('id' => 1, 'nombre' => 'Limon'), 
+           array('id' => 2, 'nombre' => 'Guacimo'),
            array('id' => 3, 'nombre' => 'Matina'),
-           array('id' => 4, 'nombre' => 'Pococí'),
+           array('id' => 4, 'nombre' => 'Pococi'),
            array('id' => 5, 'nombre' => 'Siquirres'),
            array('id' => 6, 'nombre' => 'Talamanca')
         );
