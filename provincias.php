6b60f06cc40ed22d335aadad417904a2f1cdc26a
JosueZeledon0727 <40015977+JosueZeledon0727@users.noreply.github.com>
2019-10-23 22:59:05 -0600

    Add files via upload

---
+++ provincias.php
@@ -0,0 +1,13 @@
+<?php
+ $request = json_decode(file_get_contents('php://input')); 
+ $provincias = array(
+    array('id' => 1, 'nombre' => 'San José'),
+    array('id' => 2, 'nombre' => 'Alajuela'),
+    array('id' => 3, 'nombre' => 'Cartago'),
+    array('id' => 4, 'nombre' => 'Heredia'),
+    array('id' => 5, 'nombre' => 'Guanacaste'),
+    array('id' => 6, 'nombre' => 'Puntarenas'),
+    array('id' => 7, 'nombre' => 'Limón')
+ );
+
+ echo json_encode($provincias);
